5d2603dfca0af514616bb77d025db0d4b8e3c9ba
M Keerthi vardhan <158119780+keerthivardhanm@users.noreply.github.com>
2024-12-22 14:56:03 +0530

    Delete docs directory

--- docs/.vitepress/config/social.ts
+++
@@ -1,4 +0,0 @@
-export const socialLinks = [
-  { icon: 'github', link: 'https://github.com/keerthivardhanm' },
-  { icon: 'linkedin', link: 'https://www.linkedin.com/in/m-keerthi-vardhan-47b918296' }
-]
